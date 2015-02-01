441de5560f43bba08548228f64e45fd9730cd5e4
Alexander-Derek Rein <adr@ceseros.de>
2015-02-01 23:47:12 +0100

    Credit card integration added

--- app/scripts/controllers/pay.js
+++ app/scripts/controllers/pay.js
@@ -11,5 +11,20 @@
  * Controller of the milsApp
  */
 angular.module('milsApp')
-    .controller('PayCtrl', function ($scope) {
+    .controller('PayCtrl', function ($scope, repository) {
+
+        $scope.letter = repository.letter;
+        var braintree = Braintree.create("MIIBCgKCAQEAwAPJTgb9r2B3gaWl9DILU3co12Ova1DerGuatqJbqqM5C3IOXes0L6HILz5lCD4rirWuKhMpxYus1ZuHgdiZTnuYLYe3J20ysLNW4kuX9frvsuI08+AWPY9vN/arVupVjHIgILnVScmlU0oNZMVokm+TIbob7CGkqznt4jLgO+ri4oGvkmC8f3aRwJpKrZtUScyNRcrZPa1vXvD6Z2eYt4WFMbiaWy74Oih9zlfwp+pYSib3DLsDN1a2Fys2A4sQMJMePdnlu691yLQ4yVsQbJG6bRErXu8JeC5Ws2Ku3DwwNLCnQJxwyVxyOArI2jQC3mLUtMf+X74GbPnXdMTiVQIDAQAB");
+        braintree.onSubmitEncryptForm('braintree-payment-form');
+
+        $scope.submit = function submit() {
+            console.log("submitted");
+            console.log(repository.letter.creditcard);
+            braintree.encryptForm('braintree-payment-form');
+            var content = $('#braintree-payment-form').serializeArray().reduce(function(obj, item) {
+                obj[item.name] = item.value;
+                return obj;
+            }, {});
+            console.log(content);
+        }
     });
