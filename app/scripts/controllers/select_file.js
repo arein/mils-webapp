f0b5f1c13c49fa94b5580bf2b51eea7aac4bf3a2
Alexander-Derek Rein <adr@ceseros.de>
2015-02-01 09:52:09 +0100

    Implemented File Upload Basics

--- app/scripts/controllers/select_file.js
+++ app/scripts/controllers/select_file.js
@@ -1,9 +1,22 @@
 angular.module('milsApp')
-  .controller('SelectFileCtrl', function ($scope, $location, $window, $rootScope) {
+  .controller('SelectFileCtrl', ['$scope', '$location', '$window', '$rootScope', '$upload', function ($scope, $location, $window, $rootScope, $upload) {
+  	console.log("go");
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
     $rootScope.bodyClass = "bg-letter";
-  });
+
+    $scope.$watch('files', function() {
+      $scope.upload = $upload.upload({
+        url: 'http://localhost:3000/letters',
+        data: {myObj: $scope.myModelObj},
+        file: $scope.files
+      }).progress(function(evt) {
+        console.log('progress: ' + parseInt(100.0 * evt.loaded / evt.total) + '% file :'+ evt.config.file.name);
+      }).success(function(data, status, headers, config) {
+        console.log('file ' + config.file.name + 'is uploaded successfully. Response: ' + data);
+      });
+    });
+  }]);
