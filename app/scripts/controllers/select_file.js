2de2d13dc8135aff3bb65958722f932c08767f42
Alexander-Derek Rein <adr@ceseros.de>
2015-02-06 16:41:26 +0100

    File Validation Added

--- app/scripts/controllers/select_file.js
+++ app/scripts/controllers/select_file.js
@@ -3,7 +3,37 @@ angular.module('milsApp')
     $rootScope.bodyClass = "bg-letter";
 
     $scope.$watch('files', function() {
+      $scope.error = "";
       repository.files = $scope.files;
+
+        console.log($scope.files);
+
+        if (typeof $scope.files === "undefined") {
+            return; // base Case
+        }
+
+        if ($scope.files.length == 0) {
+            $scope.error = "You have to select at least one Pdf.";
+            return;
+        }
+
+        if ($scope.files.length > 1) {
+            $scope.error = "You may not select more than one file.";
+            return;
+        }
+
+        var file = $scope.files[0];
+        console.log(file);
+
+        if (file.type != "application/pdf") {
+            $scope.error = "The file you've selected does not appear to be a Pdf.";
+            return;
+        }
+
+        if (file.size > 2000000) {
+            $scope.error = "The selected file is too big. We currently only support Pdfs up to 2mb.";
+            return;
+        }
 
         var reader = new FileReader();
 
